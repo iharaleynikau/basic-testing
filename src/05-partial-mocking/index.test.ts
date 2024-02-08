5e418ce710e2ff1f107a6a726d87fa8c8b340153
Igor Aleynikov <iharaleynikov@gmail.com>
2024-02-08 13:17:09 +0300

    partial mocking test functional

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,16 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    ...originalModule,
+    mockOne: () => void 0,
+    mockTwo: () => void 0,
+    mockThree: () => void 0,
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +19,16 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    const logSpy = jest.spyOn(global.console, 'log');
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(logSpy).toHaveBeenCalledTimes(0);
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const logSpy = jest.spyOn(global.console, 'log');
+    unmockedFunction();
+    expect(logSpy).toHaveBeenCalled();
   });
 });
