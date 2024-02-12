0022c177e4e613120dec78001806f199b04c3bbc
Igor Aleynikov <iharaleynikov@gmail.com>
2024-02-12 13:04:56 +0300

    fake timers test

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -1,52 +1,68 @@
 // Uncomment the code below and write your tests
-// import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
+import { doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
-  });
-
-  afterAll(() => {
-    jest.useRealTimers();
-  });
+  jest.useFakeTimers();
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setTimeout');
+    const callback = jest.fn();
+
+    doStuffByTimeout(callback, 1000);
+
+    expect(setTimeout).toHaveBeenLastCalledWith(callback, 1000);
   });
 
   test('should call callback only after timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setTimeout');
+    const callback = jest.fn();
+
+    doStuffByTimeout(callback, 1000);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(1000);
+
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 });
 
 describe('doStuffByInterval', () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
-  });
-
-  afterAll(() => {
-    jest.useRealTimers();
-  });
+  jest.useFakeTimers();
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    jest.spyOn(global, 'setInterval');
+    const callback = jest.fn();
+
+    doStuffByInterval(callback, 1000);
+
+    expect(setInterval).toHaveBeenLastCalledWith(callback, 1000);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    // Write your test here
+    jest.spyOn(global, 'setInterval');
+    const callback = jest.fn();
+
+    doStuffByInterval(callback, 1000);
+
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(2000);
+
+    expect(callback).toHaveBeenCalledTimes(2);
   });
 });
 
-describe('readFileAsynchronously', () => {
-  test('should call join with pathToFile', async () => {
-    // Write your test here
-  });
+// describe('readFileAsynchronously', () => {
+//   test('should call join with pathToFile', async () => {
+//     // Write your test here
+//   });
 
-  test('should return null if file does not exist', async () => {
-    // Write your test here
-  });
+//   test('should return null if file does not exist', async () => {
+//     // Write your test here
+//   });
 
-  test('should return file content if file exists', async () => {
-    // Write your test here
-  });
-});
+//   test('should return file content if file exists', async () => {
+//     // Write your test here
+//   });
+// });
